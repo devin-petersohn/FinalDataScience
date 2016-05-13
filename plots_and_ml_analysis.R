a1d0cb7f330478a56229798b175b7294aa4c1995
Devin Petersohn <devin-petersohn@users.noreply.github.com>
2016-05-13 01:33:18 -0500

    Delete plots_and_ml_analysis.R

--- plots_and_ml_analysis.R
+++
@@ -1,7 +0,0 @@
-twitter_data <- read.csv("intermediate_data.txt", encoding="UTF-*", stringsAsFactors = FALSE, header = FALSE)
-twitter_data[,5] <- NULL
-colnames(twitter_data) <- c("Month", "Day", "Year", "Count")
-
-library(ggplot2)
-
-qplot(twitter_data$Month, twitter_data$Count)
